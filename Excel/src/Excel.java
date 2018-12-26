17450d5e4b978f92874920e67541a76960cb844a
tran1619 <tran1619@gmail.com>
2018-12-26 14:40:37 -0500

    Replaced Iterator to For loop

--- Excel/src/Excel.java
+++ Excel/src/Excel.java
@@ -2,15 +2,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
-
 import javax.swing.JFileChooser;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.util.NumberToTextConverter;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
@@ -24,23 +22,45 @@ public class Excel {
 				
 			try {
 					XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(fileChooser.getSelectedFile()));
-					XSSFSheet sheet = workbook.getSheetAt(1);
-					Iterator<Row> rowIterator = sheet.rowIterator();
-					while(rowIterator.hasNext()) {
-						Row row = rowIterator.next();
-						Iterator<Cell> cellIterator = row.cellIterator();
-						while(cellIterator.hasNext()) {
-							Cell cell = cellIterator.next();
-
-							System.out.println(cell.getCellType() + "\t\t");
-
-							}
-							else if (ct == Cell.CELL_TYPE_NUMERIC) {
-								String str = NumberToTextConverter.toText(cell.getNumericCellValue())
-								System.out.println(str + "\t\t");
+					FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+					
+					// Sheet sheet = workbook.getSheetAt(1);
+//					Iterator<Row> rowIterator = sheet.rowIterator();
+//					while(rowIterator.hasNext()) {
+//						Row row = rowIterator.next();
+//						Iterator<Cell> cellIterator = row.cellIterator();
+//						while(cellIterator.hasNext()) {
+//							Cell cell = cellIterator.next();
+					
+				for (Sheet sheet : workbook) {
+					for (Row row : sheet) {
+						for (Cell cell : row) {
+							switch (cell.getCellType()) {
+							case STRING:
+								System.out.print(cell.getStringCellValue() + "\t");
+								break;
+							case NUMERIC:
+								if (DateUtil.isCellDateFormatted(cell)) {
+									System.out.print(cell.getRichStringCellValue().getString() + "\t");
+								} else {
+									System.out.print(cell.getNumericCellValue() + "\t");
+								}
+								break;
+							case BOOLEAN:
+								System.out.print(cell.getBooleanCellValue() + "\t");
+								break;
+							case FORMULA:
+								System.out.print(evaluator.evaluateFormulaCell(cell) + "\t");
+								break;
+							case BLANK:
+								System.out.print("\t");
+								break;
+							default:
+								System.out.println();
 							}
 						}
 					}
+				}
 					workbook.close();
 			
 			} catch (FileNotFoundException e) {
